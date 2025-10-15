83759e75a534b141bd90d5e50f22c74ae0e43eb4
Chris <cbrowning3422@gmail.com>
2025-10-15 03:06:41 -0400

    .

--- init.lua
+++ init.lua
@@ -1,4 +1,5 @@
 vim.opt.number = true          -- Absolute line numbers ALWAYS
 vim.opt.relativenumber = false -- No relative
 vim.opt.signcolumn = "auto:2"  -- Gutter for signs (errors) without shrinking text; "auto:2" reserves 2 colsrequire
+require("core.options")
 require("core.plugins")
