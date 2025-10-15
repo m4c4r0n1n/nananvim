b8c6fba20c945a393029851b532bb5c42d754a8b
Chris <cbrowning3422@gmail.com>
2025-10-14 23:22:45 -0400

    nananvim

---
+++ init.lua
@@ -0,0 +1,4 @@
+vim.opt.number = true          -- Absolute line numbers ALWAYS
+vim.opt.relativenumber = false -- No relative
+vim.opt.signcolumn = "auto:2"  -- Gutter for signs (errors) without shrinking text; "auto:2" reserves 2 colsrequire
+require("core.plugins")
