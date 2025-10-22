b089584e2d631a404e393e16b2239d0fe0f0c48e
Chris <cbrowning3422@gmail.com>
2025-10-22 02:05:51 -0400

    Mason fix

--- lua/config/init.lua
+++ lua/config/init.lua
@@ -1,3 +1,9 @@
+vim.api.nvim_create_autocmd("VimEnter", {
+  callback = function()
+    require("mason")
+    require("mason-lspconfig")
+  end,
+})
 require("config.options")
 require("config.keymaps")
 require("config.autocmds")
