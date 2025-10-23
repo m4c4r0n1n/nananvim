c5d7eb0b5dbc18b109de34e678156c1e71ca8cd6
Chris <cbrowning3422@gmail.com>
2025-10-23 19:46:04 -0400

    fingers crossed

---
+++ lua/config/commands.lua
@@ -0,0 +1,11 @@
+-- Custom user commands
+
+vim.api.nvim_create_user_command("TokenCount", function()
+  local text = table.concat(vim.api.nvim_buf_get_lines(0, 0, -1, false), "\n")
+  local cmd = {
+    "python3", "-c",
+    "import sys, tiktoken; enc = tiktoken.get_encoding('cl100k_base'); print(len(enc.encode(sys.stdin.read())))"
+  }
+  local result = vim.fn.system(cmd, text)
+  print("Tokens: " .. vim.trim(result))
+end, {})
